eaf66ede6d9e56808d9b63c7284776f8b57a3d96
Utsav <utsavdeep01@gmail.com>
2022-12-09 19:57:59 +0530

    Add Url loader method

--- loaders/url.ts
+++ loaders/url.ts
@@ -1,6 +1,32 @@
-export interface IUrl = {
-  id ?: string;
-  label: string;
-  url: string;
-  basicsId: string;
+import { PrismaClient, Url } from '@prisma/client';
+
+export class UrlLoader {
+  private prisma: PrismaClient;
+
+  constructor(options: { prisma: PrismaClient }) {
+    this.prisma = options.prisma;
+  }
+
+  async getUrl(id: string): Promise<Url> {
+    const result = await this.prisma.url.findUniqueOrThrow({
+      where: {
+        id: id,
+      },
+    });
+    return result;
+  }
+
+  async updateUrl(
+    id: string,
+    data: { label: string; url: string }
+  ): Promise<Url> {
+    const result = await this.prisma.url.update({
+      where: { id: id },
+      data: {
+        label: data.label,
+        url: data.url,
+      },
+    });
+    return result;
+  }
 }
